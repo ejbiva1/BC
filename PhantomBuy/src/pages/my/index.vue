bcf363cb0c770b053a528d8c0039402a24bd2257
rdai2 <rui.dai@dxc.com>
2019-04-29 18:34:41 +0800

    1. 我的 页面样式调整

--- PhantomBuy/src/pages/my/index.vue
+++ PhantomBuy/src/pages/my/index.vue
@@ -3,41 +3,43 @@
 
     <div class="row">
       <div class="userImg">
-      <open-data class="thumb" type="userAvatarUrl"></open-data>
+        <open-data class="thumb" type="userAvatarUrl"></open-data>
       </div>
       <div class="userName">
-      <open-data class="nackname" lang="zh_CN" type="userNickName"></open-data>
+        <open-data class="nackname" lang="zh_CN" type="userNickName"></open-data>
       </div>
     </div>
-    <div class="dataBody">
-      <view class="myDataBlock" @click="toMyOrderPage">
-        <view class="address_sort">
+    <div class="myDataBlock">
+      <wxc-panel :border="has_border" @click="toMyOrderPage">
+
+        <view class="address_sort padding">
           <view>我的订单</view>
           <view>
-            <wxc-icon color="#red" size="30" type="arrow-right"></wxc-icon>
+            <wxc-icon color="#red" size="25" type="arrow-right"></wxc-icon>
           </view>
-        </view>
-      </view>
 
-      <view class="myDataBlock" @click="toMyAddressPage">
-        <view class="address_sort">
+        </view>
+      </wxc-panel>
+      <wxc-panel :border="has_border" @click="toMyAddressPage">
+        <view class="address_sort padding">
           <view>收货地址管理</view>
           <view>
-            <wxc-icon color="#red" size="30" type="arrow-right"></wxc-icon>
+            <wxc-icon color="#red" size="25" type="arrow-right"></wxc-icon>
           </view>
         </view>
-      </view>
+      </wxc-panel>
 
 
     </div>
-    <!--<div class="signOut">退出账号</div>-->
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   export default {
     data(){
-      return {};
+      return {
+        has_border: true
+      };
     },
     methods: {
       toMyAddressPage(){
@@ -54,7 +56,15 @@
   }
 </script>
 
-<style scoped>
+<style>
+  .my {
+    background-color: #F7F7F7;
+    height: 100%;
+    width: 100%;
+    position: fixed;
+    font-size: 0.23rem;
+  }
+
   .userImg {
     margin-top: 0.20rem;
     margin-left: 0.20rem;
@@ -67,39 +77,9 @@
     margin-left: 0.20rem;
   }
 
-  .my {
-    background-color: #F7F7F7;
-    position: fixed;
-    height: 100%;
-    width: 100%;
-  }
-
-  .dataBody {
-  }
-
-  .navLink {
-    margin-left: 0.20rem;
-    margin-right: 0.20rem;
-  }
-
-  .myOrderDataBlock {
-    background-color: white;
-    margin-top: 0.20rem;
-    padding-top: 0.20rem;
-    padding-bottom: 0.20rem;
-    padding-left: 0.20rem;
-    padding-right: 0.20rem;
-  }
-
   .myDataBlock {
-    background-color: white;
-    margin-top: 0.20rem;
-    margin-left: 0.20rem;
-    margin-right: 0.20rem;
-    padding-top: 0.20rem;
-    padding-bottom: 0.20rem;
-    padding-left: 0.20rem;
-    padding-right: 0.20rem;
+    width: 100%;
+    padding: 0rem 0.20rem 0rem 0rem;
   }
 
   .row {
@@ -107,36 +87,13 @@
     flex-direction: row;
   }
 
-  .halfRow {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    width: 60%;
-  }
-
-  .twoSideRow {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-  }
-
-  .myDataLink {
-    color: black;
-  }
-
   .address_sort {
     display: flex;
     justify-content: space-between;
   }
 
-  .discountCla {
-    color: gray;
+  .padding {
+    padding: 0.2rem 0.1rem 0.1rem 0.25rem;
   }
 
-  .adData {
-    color: gray;
-    font-size: smaller;
-  }
 </style>
--- PhantomBuy/src/pages/site/index.vue
+++ PhantomBuy/src/pages/site/index.vue
@@ -118,7 +118,6 @@
   export default {
     data() {
       return {
-        b: 'a',
         ListSiteProductCategory: [],
         site_product_category_list: [],
         site_no_product_category_list: [],
