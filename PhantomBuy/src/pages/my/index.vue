bcfbda6dc776b740790f12ea25433199dde20b22
rdai2 <rui.dai@dxc.com>
2019-05-27 11:08:14 +0800

    转发 低配版实现

--- PhantomBuy/src/pages/my/index.vue
+++ PhantomBuy/src/pages/my/index.vue
@@ -48,6 +48,25 @@
         has_border: true
       };
     },
+    /**
+     * 进行页面分享
+     */
+    onShareAppMessage: function (options) {
+      if (options.from === 'button') {
+        // 来自页面内转发按钮
+        console.log(options.target)
+      }
+      return {
+        //## 此为转发给微信好友或微信群后，对方点击后进入的页面链接，可以根据自己的需求添加参数
+//        path: 'pages/index/index',
+        success: function (res) {
+          //这是我自定义的函数，可替换自己的操作
+        },
+        //## 转发操作失败/取消 后的回调处理，一般是个提示语句即可
+        fail: function () {
+        }
+      }
+    },
     methods: {
       toMyAddressPage(){
         wx.navigateTo({
--- PhantomBuy/src/pages/site/index.vue
+++ PhantomBuy/src/pages/site/index.vue
@@ -242,6 +242,25 @@
       this.loadReachBottomList();
     },
     computed: {},
+    /**
+     * 进行页面分享
+     */
+    onShareAppMessage: function (options) {
+      if (options.from === 'button') {
+        // 来自页面内转发按钮
+        console.log(options.target)
+      }
+      return {
+        //## 此为转发给微信好友或微信群后，对方点击后进入的页面链接，可以根据自己的需求添加参数
+//        path: 'pages/index/index',
+        success: function (res) {
+          //这是我自定义的函数，可替换自己的操作
+        },
+        //## 转发操作失败/取消 后的回调处理，一般是个提示语句即可
+        fail: function () {
+        }
+      }
+    },
     methods: {
       // 显示优惠信息
       set_site_promotion_list(){
